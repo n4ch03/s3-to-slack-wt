20f88060d090f855dc63164aeeb30306658cd9a3
Ignacio Esmite <iesmite@gmail.com>
2016-07-31 22:12:31 -0300

    Swagger and updates to webtask

--- wt.js
+++ wt.js
@@ -16,25 +16,14 @@ app.use(json());
 
 
 // GET
-app.get('/slack', (req, res) => {
-  console.log(req.webtaskContext.secrets.SLACK_TOKEN);
-  let slackClient = new Slack(req.webtaskContext.secrets.SLACK_TOKEN);
-  slackClient.api('chat.postMessage', {
-    text:'from webtask',
-    channel:'#test-automation-fo',
-    attachments: 	'[{"pretext": "pre-hello", "text": "text-world"}]'
-  }, function(err, response){
-    console.log(response);
-    res.send('OK!');    
-  });
-});
-
-
-// GET
 app.get('/getFiles', (req, res) => {
   AWS.config.update({accessKeyId: req.webtaskContext.secrets.AWS_ACCESS_KEY_ID, secretAccessKey:req.webtaskContext.secrets.AWS_SECRET_ACCESS_KEY});
   let s3 = new AWS.S3();
-  const path = req.query.path || '';
+  const path = req.query.path || 'functional-tests/4979';
+  const channel = '#' + (req.query.channel || 'test-automation-fo');
+  console.log(`Path is: ${path}`);
+  console.log(`Channel is: ${channel}`);
+
   const params = {
     Bucket: req.webtaskContext.secrets.AWS_BUCKET,
     Prefix: path
@@ -49,22 +38,30 @@ app.get('/getFiles', (req, res) => {
       data.Contents.forEach((obj) => {
         let fname = obj.Key;
         results.push(fname);
+        const fnameUrl = fname.replace('+', '%2b');
         attachments.push({
             "title": fname.replace(path + '/', ''),
-            "title_link": "https://webtask.it.auth0.com/api/run/wt-iesmite-gmail_com-0/doppio-image/file?path="+fname,
-            "mrkdwn_in": ["text"]
+            "title_link": `https://webtask.it.auth0.com/api/run/${req.x_wt.container}/${req.x_wt.jtn}/file?path=${fnameUrl}`,
+            "mrkdwn_in": ["text"],
+            "color": "#e60b25",
         });
-
       });
       let slackClient = new Slack(req.webtaskContext.secrets.SLACK_TOKEN);
-      slackClient.api('chat.postMessage', {
-        text:'Screenshots for failed tests :facepalm:',
-        channel:'#test-automation-fo',
-        "attachments": 	JSON.stringify(attachments)
-      }, function(err, response){
-        console.log(response);
-        res.send('OK!');    
-      });
+      const splittedPath = path.split('/');
+      const message = `${splittedPath[0]}, build: ${splittedPath[1]} : ${req.webtaskContext.secrets.SLACK_MESSAGE}`;
+
+      if (attachments.length > 0) {
+        slackClient.api('chat.postMessage', {
+          text:message,
+          channel: channel,
+          "attachments": 	JSON.stringify(attachments)
+        }, function(err, response){
+          console.log(response);
+          res.json({ ok: 'OK!' });
+        });
+      } else {
+        res.json({ ok: 'OK: No tests to send!' });
+      }
     }  
   });
 });
@@ -80,21 +77,61 @@ app.get('/file', (req, res) => {
     Bucket: req.webtaskContext.secrets.AWS_BUCKET,
     Key: path
   };
-  
-  /*s3.getObject(params, function(err, data) {
-    if (err) {
-      console.log(err, err.stack); // an error occurred
-      res.json({ error: err });
-    } else  {
-      console.log(data);
-      res.pipe(data.Body);
-    }
-  });*/
+
   let imgStream = s3.getObject(params).createReadStream();
   imgStream.pipe(res);
   
 });
 
+app.get('/listByPath', (req, res) => {
+  AWS.config.update({accessKeyId: req.webtaskContext.secrets.AWS_ACCESS_KEY_ID, secretAccessKey:req.webtaskContext.secrets.AWS_SECRET_ACCESS_KEY});
+  let s3 = new AWS.S3();
+  const path = req.query.path || 'functional-tests/4979';
+
+  const params = {
+    Bucket: req.webtaskContext.secrets.AWS_BUCKET,
+    Prefix: path
+  };
+
+  s3.listObjects(params, function(err, data) {
+    if (err) {
+      console.log(err, err.stack); // an error occurred
+      res.json({ error: err });
+    } else {     
+      var results = [];
+      data.Contents.forEach((obj) => {
+        let key  = obj.Key.replace('+', '%2b');
+        results.push({
+          'key': obj.Key,
+          'delete': `https://webtask.it.auth0.com/api/run/${req.x_wt.container}/${req.x_wt.jtn}/deleteByKey?key=${obj.Key}`
+        });
+      });
+      res.json(results);
+    }  
+  });
+});
+
+//Yes yes I know is not the right way is a get to a delete :)
+
+app.get('/deleteByKey', (req, res) => {
+  AWS.config.update({accessKeyId: req.webtaskContext.secrets.AWS_ACCESS_KEY_ID, secretAccessKey:req.webtaskContext.secrets.AWS_SECRET_ACCESS_KEY});
+  let s3 = new AWS.S3();
+  const key = req.query.key;
+
+  const params = {
+    Bucket: req.webtaskContext.secrets.AWS_BUCKET,
+    Key: key
+  };
+
+  s3.deleteObject(params, function(err, data) {
+    if (err) {
+      console.log(err, err.stack); // an error occurred
+      res.json({ error: err });
+    } else {     
+      res.json({result: 'OK'});
+    }
+  });  
+});
 
 // expose this express app as a webtask-compatible function
 module.exports = fromExpress(app);
